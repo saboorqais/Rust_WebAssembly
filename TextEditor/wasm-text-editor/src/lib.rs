5fdfac66f3e2d3deceb677f3cd95367fb21cbc29
saboorqais <saboor.qaiser456@gmail.com>
2025-03-09 20:38:09 +0500

    Added Markdown Imporvement in the text Ediotr

--- TextEditor/wasm-text-editor/src/lib.rs
+++ TextEditor/wasm-text-editor/src/lib.rs
@@ -1,8 +1,20 @@
 use wasm_bindgen::prelude::*;
-
+use pulldown_cmark::{Parser, Options, html};
 // Expose functions to JavaScript
 #[wasm_bindgen]
 pub fn format_text(text: &str, tag: &str) -> String {
     format!("<{}>{}</{}>", tag, text, tag)
 }
 
+
+
+// Expose function to JavaScript
+#[wasm_bindgen]
+pub fn format_markdown(input: &str) -> String {
+    let mut options = Options::empty();
+    let parser = Parser::new_ext(input, options);
+    
+    let mut output = String::new();
+    html::push_html(&mut output, parser);
+    output
+}
