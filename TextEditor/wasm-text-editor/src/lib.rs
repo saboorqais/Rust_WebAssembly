e92fa4fb199a7ec5ac37dc9533ce8a9240470e11
saboorqais <saboor.qaiser456@gmail.com>
2025-03-09 21:03:27 +0500

    Added Color High light for this

--- TextEditor/wasm-text-editor/src/lib.rs
+++ TextEditor/wasm-text-editor/src/lib.rs
@@ -1,10 +1,30 @@
 use wasm_bindgen::prelude::*;
 use pulldown_cmark::{Parser, Options, html};
 // Expose functions to JavaScript
+
+const KEYWORDS: [&str; 5] = ["fn", "let", "if", "else", "return"];
+
+#[wasm_bindgen]
+pub fn highlight_code(input :&str)->String{
+    let mut output = String::new();
+   
+    for word in input.split_whitespace(){
+        if KEYWORDS.contains(&word){
+            output.push_str(&format!("<span style='color: blue;'>{}</span> ", word));
+        }else{
+            output.push_str(&format!("{} ", word));
+        }
+        output.push(' ');
+    }
+    output.trim().to_string()
+}
+
 #[wasm_bindgen]
 pub fn format_text(text: &str, tag: &str) -> String {
     format!("<{}>{}</{}>", tag, text, tag)
 }
+
+
 
 
 
