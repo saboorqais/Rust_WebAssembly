fca9328761f16f709e1180a4b89020ec6a421a77
saboorqais <saboor.qaiser456@gmail.com>
2025-04-21 00:26:47 +0500

    Added Expiry

---
+++ redis/src/main.rs
@@ -0,0 +1,107 @@
+use std::{
+    collections::HashMap,
+    io::{BufRead, BufReader, Write},
+    net::{TcpListener, TcpStream},
+    sync::{Arc, Mutex},
+    thread,
+};
+use chrono::{Utc,Duration,DateTime};
+type Db = Arc<Mutex<HashMap<String, String>>>;
+type CACHE = Arc<Mutex<HashMap<String, DateTime<Utc>>>>;
+
+fn handle_client(stream: TcpStream, db: Db,cache:CACHE) {
+    let mut reader = BufReader::new(stream.try_clone().unwrap());
+    let mut writer = stream;
+
+    loop {
+        let mut input = String::new();
+        if reader.read_line(&mut input).is_err() {
+            break;
+        }
+
+        println!("{:?}", input.trim().split_whitespace().collect::<Vec<_>>());
+        println!("{:?}",  db.lock().unwrap());
+        println!("{:?}",  cache.lock().unwrap());
+        let parts: Vec<&str> = input.trim().split_whitespace().collect();
+        if parts.is_empty() {
+            continue;
+        }
+
+        let response:String = match parts[0].to_uppercase().as_str() {
+            "SET" if parts.len() == 3 => {
+                db.lock()
+                    .unwrap()
+                    .insert(parts[1].to_string(), parts[2].to_string());
+                cache.lock().unwrap().insert(parts[1].to_string(),Utc::now() + Duration::seconds(parts[2].parse::<i64>().unwrap()));
+                "+OK\n".to_string()
+            }
+            "EXPIRE" if parts.len() ==3  => {
+             
+                let key: &str = parts[1];
+                if db.lock().unwrap().get(key).is_none()  {
+                    let message  =  "+Value Does Not Exist\n".to_string() ;
+                    message;// Early return if key missing
+                }
+         
+            
+                let mut cache = cache.lock().unwrap();
+                let duration = Duration::seconds(parts[2].parse::<i64>().unwrap());
+            
+                let later = match cache.get(key) {
+                    Some(existing) => *existing + duration,
+                    None => Utc::now() + duration,
+                };
+            
+                cache.insert(key.to_string(), later);
+            
+                "+OK\n".to_string()
+         
+            }
+            "GET" if parts.len() == 2 && parts[1] == "*" => {
+                let db = db.lock().unwrap();
+                if db.is_empty() {
+                    "$-1\n".to_string()
+                } else {
+                    let mut response = String::new();
+                    for (k, v) in db.iter() {
+                        response.push_str(&format!("{}: {}\n", k, v));
+                    }
+                    response
+                }
+            }
+            "GET" if parts.len() == 2 => match db.lock().unwrap().get(parts[1]) {
+                Some(val) => format!("+{}\n", val),
+                None => "$-1\n".to_string(),
+            },
+            "DEL" if parts.len() == 2 => {
+                let removed = db.lock().unwrap().remove(parts[1]);
+                format!(":{}\n", if removed.is_some() { 1 } else { 0 })
+            }
+            "EXIT" => break,
+            _ => "-ERR unknown command\n".to_string(),
+        };
+
+        let _ = writer.write_all(response.as_bytes());
+    }
+}
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:6380").unwrap();
+    let db: Db = Arc::new(Mutex::new(HashMap::new()));
+    let cache:CACHE = Arc::new(Mutex::new(HashMap::new()));
+
+    println!("Mini Redis clone running on port 6379");
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                let db = Arc::clone(&db);
+                let cache = Arc::clone(&cache);
+                thread::spawn(move || {
+                    handle_client(stream, db,cache);
+                });
+            }
+            Err(e) => eprintln!("Connection failed: {}", e),
+        }
+    }
+}
