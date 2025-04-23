f9ed709034c69c74c4b5abb7db1d039cb72848ef
saboorqais <saboor.qaiser456@gmail.com>
2025-04-23 22:49:04 +0500

    added LPUSH Complete funcitonlaity

--- redis/src/main.rs
+++ redis/src/main.rs
@@ -1,16 +1,15 @@
 use std::{
-    collections::{HashMap,HashSet},
+    collections::{HashMap, HashSet},
     io::{BufRead, BufReader, Write},
     net::{TcpListener, TcpStream},
     sync::{Arc, Mutex},
     thread,
 };
 mod types;
+use chrono::{Duration, Utc};
 use types::*;
-use chrono::{Utc,Duration};
 
-
-fn handle_client(stream: TcpStream, db: Db,cache:CACHE) {
+fn handle_client(stream: TcpStream, db: Db, cache: CACHE) {
     let mut reader = BufReader::new(stream.try_clone().unwrap());
     let mut writer = stream;
 
@@ -21,68 +20,101 @@ fn handle_client(stream: TcpStream, db: Db,cache:CACHE) {
         }
 
         println!("{:?}", input.trim().split_whitespace().collect::<Vec<_>>());
-        println!("{:?}",  db.lock().unwrap());
-        println!("{:?}",  cache.lock().unwrap());
+        println!("{:?}", db.lock().unwrap());
+        println!("{:?}", cache.lock().unwrap());
         let parts: Vec<&str> = input.trim().split_whitespace().collect();
         if parts.is_empty() {
             continue;
         }
 
-        let response:String = match parts[0].to_uppercase().as_str() {
+        let response: String = match parts[0].to_uppercase().as_str() {
             "SET" if parts.len() == 3 => {
-                db.lock()
+                db.lock().unwrap().insert(
+                    parts[1].to_string(),
+                    RedisValue {
+                        value: ValueType::String((parts[2].to_string())),
+                    },
+                );
+                cache
+                    .lock()
                     .unwrap()
-                    .insert(parts[1].to_string(), RedisValue { value:ValueType::String((parts[2].to_string()))  });
-                cache.lock().unwrap().insert(parts[1].to_string(),Utc::now() + Duration::seconds(144000));
+                    .insert(parts[1].to_string(), Utc::now() + Duration::seconds(144000));
                 "+OK\n".to_string()
             }
-            "LPUSH" if parts.len() == 3 =>{
-                let key: &str = parts[1];
-                if db.lock().unwrap().get(key).is_none()  {
-                    let newLinkedList =  LinkedList::new(parts[2].to_string());
-                    db.lock()
-                    .unwrap()
-                    .insert(parts[1].to_string(), RedisValue { value:ValueType::LinkedList(newLinkedList)  });
-                "+Key Created\n".to_string();
-                }
-                if let Some(redis_value) = db.lock().unwrap().get_mut(key) {
+            "LPUSH" if parts.len() == 3 => {
+                let key = parts[1];
+                let value = parts[2].to_string();
+                let mut db = db.lock().unwrap();
+
+                if let Some(redis_value) = db.get_mut(key) {
                     match &mut redis_value.value {
                         ValueType::LinkedList(list) => {
-                            list.append(parts[2].to_string());
+                            list.append(value);
+                            "+OK\n".to_string()
                         }
-                        _ => {
-                            // Handle the case where the key exists but is not a List
-                            println!("Type error: Value is not a List!");
-                        }
+                        _ => "-ERR wrong type\n".to_string(),
                     }
                 } else {
-                    println!("Key not found!");
+                    let mut list = LinkedList::new(value);
+                    db.insert(
+                        key.to_string(),
+                        RedisValue {
+                            value: ValueType::LinkedList(list),
+                        },
+                    );
+                    cache
+                        .lock()
+                        .unwrap()
+                        .insert(key.to_string(), Utc::now() + Duration::seconds(144000));
+                    "+Key Created\n".to_string()
                 }
-            
-                "+OK\n".to_string()
-         
             }
-            "EXPIRE" if parts.len() ==3  => {
-             
-                let key: &str = parts[1];
-                if db.lock().unwrap().get(key).is_none()  {
-                    let message  =  "+Value Does Not Exist\n".to_string() ;
-                    message;// Early return if key missing
+            "LPOP" if parts.len() == 3 => {
+                let key = parts[1];
+                let value = parts[2].to_string();
+                let mut db = db.lock().unwrap();
+
+                if let Some(redis_value) = db.get_mut(key) {
+                    match &mut redis_value.value {
+                        ValueType::LinkedList(list) => {
+                            list.append(value);
+                            "+OK\n".to_string()
+                        }
+                        _ => "-ERR wrong type\n".to_string(),
+                    }
+                } else {
+                    let mut list = LinkedList::new(value);
+                    db.insert(
+                        key.to_string(),
+                        RedisValue {
+                            value: ValueType::LinkedList(list),
+                        },
+                    );
+                    cache
+                        .lock()
+                        .unwrap()
+                        .insert(key.to_string(), Utc::now() + Duration::seconds(144000));
+                    "+Key Created\n".to_string()
                 }
-         
-            
+            }
+            "EXPIRE" if parts.len() == 3 => {
+                let key: &str = parts[1];
+                if db.lock().unwrap().get(key).is_none() {
+                    let message = "+Value Does Not Exist\n".to_string();
+                    message; // Early return if key missing
+                }
+
                 let mut cache = cache.lock().unwrap();
                 let duration = Duration::seconds(parts[2].parse::<i64>().unwrap());
-            
+
                 let later = match cache.get(key) {
                     Some(existing) => *existing + duration,
                     None => Utc::now() + duration,
                 };
-            
+
                 cache.insert(key.to_string(), later);
-            
+
                 "+OK\n".to_string()
-         
             }
             "GET" if parts.len() == 2 && parts[1] == "*" => {
                 let db = db.lock().unwrap();
@@ -115,7 +147,7 @@ fn handle_client(stream: TcpStream, db: Db,cache:CACHE) {
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6380").unwrap();
     let db: Db = Arc::new(Mutex::new(HashMap::new()));
-    let cache:CACHE = Arc::new(Mutex::new(HashMap::new()));
+    let cache: CACHE = Arc::new(Mutex::new(HashMap::new()));
 
     println!("Mini Redis clone running on port 6379");
 
@@ -125,7 +157,7 @@ fn main() {
                 let db = Arc::clone(&db);
                 let cache = Arc::clone(&cache);
                 thread::spawn(move || {
-                    handle_client(stream, db,cache);
+                    handle_client(stream, db, cache);
                 });
             }
             Err(e) => eprintln!("Connection failed: {}", e),
